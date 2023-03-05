e07e279d72770e20fbaeaac0ab697550d8f22ef5
jis3r <jis3r@protonmail.com>
2023-03-05 15:05:15 +0100

    add: profile page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Navbar from '../../lib/components/navbar.svelte';
+</script>
+
+<main class="mt-10 max-w-4xl justify-self-center mx-auto px-4">
+	<h1 class="text-left text-3xl font-bold mb-4">Profile</h1>
+</main>
+
+<Navbar />
