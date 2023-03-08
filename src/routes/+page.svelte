717e52c9f56d511657f86a0a22837d8c2b70a78b
jis3r <jis3r@protonmail.com>
2023-03-08 10:07:34 +0100

    add: addQuote-toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,33 @@
 <script>
-	import Navbar from '../lib/components/navbar.svelte';
+	import AddQuote from '$lib/components/addQuote.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
+
+	let addQuote = false;
 </script>
 
 <main class=" h-screen mt-10 max-w-4xl justify-self-center mx-auto px-4">
 	<h1 class="text-left text-3xl font-bold mb-4">Home</h1>
-	<div class="rounded-lg p-4 shadow-md overflow-hidden my-4">
+	<!--div class="rounded-lg p-4 shadow-md overflow-hidden my-4">
 		<h3 class="w-full">Get a random Quote!</h3>
 		<button class="px-4 py-2 border-gray-900 border font-bold rounded-full mt-4 float-right">
 			Shuffle!
 		</button>
 		<div class="clearfix" />
-	</div>
+	</!--div>
 	<div />
 	<h2 class="text-left text-xl font-semibold my-4">Recently added</h2>
-	<div class="rounded-lg p-4 shadow-md my-4">Recently Added</div>
+	<div-- class="rounded-lg p-4 shadow-md my-4">Recently Added</div-->
 
-	<button class="block border-gray-900 border font-bold py-2 px-4 rounded-full w-full">
+	<button
+		on:click={() => (addQuote = !addQuote)}
+		class="block border-gray-900 bg-gray-900 text-white border font-bold py-2 px-4 rounded-full w-full"
+	>
 		Add Quote
 	</button>
+
+	{#if addQuote}
+		<AddQuote />
+	{/if}
 </main>
 
 <Navbar current="home" />
