f373805f4d00b2db0865255a1a8697fcda8a2e6e
jis3r <jis3r@protonmail.com>
2023-03-11 22:16:20 +0100

    fix: max-w applied to navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -20,31 +20,31 @@
 	};
 </script>
 
-<nav
-	class="flex justify-between w-screen fixed bottom-0 backdrop-blur-sm bg-gray-200 bg-opacity-50"
->
-	<button
-		on:click={gotoCollection}
-		class:text-svelte-orange={current === 'collection'}
-		class="w-1/3 p-1 flex flex-col justify-center items-center text-gray-900"
-	>
-		<Library />
-		<span class="mt-0.5 text-sm font-semibold">Collection</span>
-	</button>
-	<button
-		on:click={gotoHome}
-		class:text-svelte-orange={current === 'home'}
-		class="w-1/3 p-1 flex flex-col justify-center items-center text-gray-900"
-	>
-		<Home />
-		<span class="mt-0.5 text-sm font-semibold">Home</span>
-	</button>
-	<button
-		on:click={gotoProfile}
-		class:text-svelte-orange={current === 'profile'}
-		class="w-1/3 p-1 flex flex-col justify-center items-center text-gray-900"
-	>
-		<User />
-		<span class="mt-0.5 text-sm font-semibold">Profile</span>
-	</button>
+<nav class="fixed bottom-0 w-screen bg-gray-200 bg-opacity-50 backdrop-blur-sm">
+	<div class="mx-auto flex max-w-4xl justify-between">
+		<button
+			on:click={gotoCollection}
+			class:text-svelte-orange={current === 'collection'}
+			class="flex w-1/3 flex-col items-center justify-center p-1 text-gray-900"
+		>
+			<Library />
+			<span class="mt-0.5 text-sm font-semibold">Collection</span>
+		</button>
+		<button
+			on:click={gotoHome}
+			class:text-svelte-orange={current === 'home'}
+			class="flex w-1/3 flex-col items-center justify-center p-1 text-gray-900"
+		>
+			<Home />
+			<span class="mt-0.5 text-sm font-semibold">Home</span>
+		</button>
+		<button
+			on:click={gotoProfile}
+			class:text-svelte-orange={current === 'profile'}
+			class="flex w-1/3 flex-col items-center justify-center p-1 text-gray-900"
+		>
+			<User />
+			<span class="mt-0.5 text-sm font-semibold">Profile</span>
+		</button>
+	</div>
 </nav>
