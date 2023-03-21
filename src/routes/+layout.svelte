e7659f417d9a767ca745ab8ac4b54766cb7e5a8b
jis3r <jis3r@protonmail.com>
2023-03-21 13:48:39 +0100

    fix: theme-flash

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,6 @@
 	import { supabaseClient } from '$lib/supabase';
 
 	onMount(() => {
-		if (
-			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-
 		const {
 			data: { subscription }
 		} = supabaseClient.auth.onAuthStateChange(() => {
