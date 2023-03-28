a88bb9fbc14c16c23b50e7f1c2e0e22b1f7885ee
jis3r <jis3r@protonmail.com>
2023-03-28 17:41:06 +0200

    fix: optimized optionsMenu-styling

--- src/lib/components/optionsMenu.svelte
+++ src/lib/components/optionsMenu.svelte
@@ -11,12 +11,19 @@
 		showMenu ? 'block' : 'hidden'
 	} absolute right-full top-0 -mt-2 w-24 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800 dark:text-gray-100`}
 >
-	<button class="block w-full px-4 py-2 text-left text-sm" on:click={deleteHandler}>
-		<span class="float-left mr-2">Delete </span>
-		<Trash size="16" class="float-left" />
+	<button
+		class="justify-left flex w-full items-center space-x-1 px-4 py-2 text-left text-sm"
+		on:click={deleteHandler}
+	>
+		<Trash size="14" />
+		<span>Delete</span>
 	</button>
-	<button class="block w-full px-4 py-2 text-left text-sm" on:click={editHandler}>
-		<span class="float-left mr-2">Edit </span>
-		<Pen size="16" class="float-left" />
+
+	<button
+		class="justify-left flex w-full items-center space-x-1 px-4 py-2 text-left text-sm"
+		on:click={editHandler}
+	>
+		<Pen size="14" />
+		<span>Edit</span>
 	</button>
 </div>
